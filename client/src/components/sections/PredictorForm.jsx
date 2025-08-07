b5c097b56f78b7ee9787cd56d432bdce770a4c56
gayith <gayith.mxz@gmail.com>
2025-08-07 23:00:30 +0530

    Added the input parameters

--- client/src/components/sections/PredictorForm.jsx
+++ client/src/components/sections/PredictorForm.jsx
@@ -1,13 +1,8 @@
 "use client";
 
-import { Label } from "../ui/label";
-import { Input } from "../ui/input";
+import { Label } from "../ui/Labels";
+import { Input } from "../ui/Input";
 import { cn } from "../../lib/utils";
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-  IconBrandOnlyfans,
-} from "@tabler/icons-react";
 
 const PredictorForm = () => {
   
@@ -18,17 +13,16 @@ const PredictorForm = () => {
 
   return (
     <div
-      className="shadow-input mx-auto w-full max-w-md rounded-none bg-white p-4 md:rounded-2xl md:p-8 dark:bg-black">
+      className="shadow-input mx-auto w-full max-w-7xl rounded-none bg-white p-4 md:rounded-2xl md:p-8 dark:bg-black">
       <h2 className="text-xl font-bold text-neutral-800 dark:text-neutral-200">
-        Welcome to Aceternity
+        Welcome to HCare
       </h2>
-      <p className="mt-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
-        Login to aceternity if you can because we don&apos;t have a login flow
-        yet
+      <p className="mt-2 mx-auto max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
+        Fill out your details as accurately as possible
       </p>
       <form className="my-8" onSubmit={handleSubmit}>
-        <div
-          className="mb-4 flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2">
+
+        <div className="mb-4 flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2">
           <LabelInputContainer>
             <Label htmlFor="firstname">First name</Label>
             <Input id="firstname" placeholder="Tyler" type="text" />
@@ -37,31 +31,92 @@ const PredictorForm = () => {
             <Label htmlFor="lastname">Last name</Label>
             <Input id="lastname" placeholder="Durden" type="text" />
           </LabelInputContainer>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="email">Email Address</Label>
+            <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+          </LabelInputContainer>
         </div>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
-        </LabelInputContainer>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder="••••••••" type="password" />
+
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Body Mass Index (BMI)</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
         </LabelInputContainer>
         <LabelInputContainer className="mb-8">
-          <Label htmlFor="twitterpassword">Your twitter password</Label>
+          <Label htmlFor="twitterpassword">Do You Smoke?</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Do You Drink Alcohol?</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Have You Ever Had a Stroke?</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">How Many Days Did You Feel Physically Unwell in the Past 30 Days?</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">How Many Days Did You Feel Mentally Unwell in the Past 30 Days?</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Do You Have Difficulty Walking?</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Sex Assigned at Birth</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Select Your Age Group</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Select Your Race</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Do You Have Diabetes?</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Do You Exercise Regularly?</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">How Would You Rate Your General Health?</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">How Many Hours Do You Sleep on Average?</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Do You Have Asthma?</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Do You Have Kidney Disease?</Label>
+          <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Have You Ever Had Skin Cancer?</Label>
           <Input id="twitterpassword" placeholder="••••••••" type="twitterpassword" />
         </LabelInputContainer>
 
         <button
           className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
           type="submit">
-          Sign up &rarr;
+          Do I Have Heart Disease??? &rarr;
           <BottomGradient />
         </button>
 
         <div
           className="my-8 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
 
-        <div className="flex flex-col space-y-4">
+        {/* <div className="flex flex-col space-y-4">
           <button
             className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
             type="submit">
@@ -89,7 +144,7 @@ const PredictorForm = () => {
             </span>
             <BottomGradient />
           </button>
-        </div>
+        </div> */}
       </form>
     </div>
   );
