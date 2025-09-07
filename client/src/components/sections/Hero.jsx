3568ee2363ea947614b7a0af4c7c1627c293de43
gayith <gayith.mxz@gmail.com>
2025-09-07 18:07:22 +0530

    First-Version-Complete

--- client/src/components/sections/Hero.jsx
+++ client/src/components/sections/Hero.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "motion/react";
+import { Link } from "react-scroll";
 
 const Hero = () => {
   return (
@@ -62,9 +63,10 @@ const Hero = () => {
           }}
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4">
 
-          <button className="relative w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+          <Link className="hover:cursor-pointer relative w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200"
+            to="predictorform" smooth={true} duration={1500}>
             Explore Now
-          </button>
+          </Link>
 
           {/* <button className="relative w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
             Contact Support
